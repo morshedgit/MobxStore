5bf43c7d5f83136d3ea8c2c57c13019d019a15e4
Sadeq Morshedloo <sadeq.morshedloo@campus-support.ca>
2022-10-27 00:43:09 +0000

    feat: adds role based access

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,31 +1,14 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
+import * as express from "express";
+import * as cors from "cors";
+import * as bodyParser from "body-parser";
+import { routesConfig } from "./users/routes-config";
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
+admin.initializeApp();
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello to you from Firebase!");
-});
-/**
- * Gets users info
- */
-export const getUsers = functions.https.onRequest((request, response) => {
-  //   admin.auth
-  //     .getAuth()
-  //     .getUsers([])
-  //     .then((getUsersResult) => {
-  //       console.log("Successfully fetched user data:");
-  //       getUsersResult.users.forEach((userRecord) => {
-  //         console.log(userRecord);
-  //       });
-  //       console.log("Unable to find users corresponding to these identifiers:");
-  //       getUsersResult.notFound.forEach((userIdentifier) => {
-  //         console.log(userIdentifier);
-  //       });
-  //     })
-  //     .catch((error) => {
-  //       console.log("Error fetching user data:", error);
-  //     });
-});
+const app = express();
+app.use(bodyParser.json());
+app.use(cors({ origin: true }));
+routesConfig(app);
+export const api = functions.https.onRequest(app);
